ea595562af1ca722697ae1e929dcc543eb801f7a
thiagofalima <thiagofalima@hotmail.com>
2024-04-09 20:51:21 -0300

    Consulta com varios criterios

--- src/main/java/com/projetomongodb/codes/resources/PostResource.java
+++ src/main/java/com/projetomongodb/codes/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.projetomongodb.codes.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,4 +36,15 @@ public class PostResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
+	@GetMapping("/fullsearch")
+	public ResponseEntity<List<Post>> fullsearch(
+			@RequestParam(value="text", defaultValue = "") String text,
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(minDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
 }
