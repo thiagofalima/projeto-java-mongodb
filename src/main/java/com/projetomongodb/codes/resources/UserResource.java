705727e8782d446b4273778b639a8d272c289f1d
thiagofalima <thiagofalima@hotmail.com>
2024-04-09 15:59:02 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/projetomongodb/codes/resources/UserResource.java
+++ src/main/java/com/projetomongodb/codes/resources/UserResource.java
@@ -6,9 +6,17 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.projetomongodb.codes.domain.Post;
 import com.projetomongodb.codes.domain.User;
 import com.projetomongodb.codes.dto.UserDTO;
 import com.projetomongodb.codes.services.UserService;
@@ -60,5 +68,11 @@ public class UserResource {
 		obj = service.update(obj);
 		
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
