a49acf2e526424e2dc722597cb7096913775ecaa
thiagofalima <thiagofalima@hotmail.com>
2024-04-09 16:19:44 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/projetomongodb/codes/resources/PostResource.java
+++ src/main/java/com/projetomongodb/codes/resources/PostResource.java
@@ -1,23 +1,13 @@
 package com.projetomongodb.codes.resources;
 
-import java.net.URI;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.projetomongodb.codes.domain.Post;
-import com.projetomongodb.codes.domain.User;
-import com.projetomongodb.codes.dto.UserDTO;
 import com.projetomongodb.codes.services.PostService;
 
 @RestController
